e4b6be3da9a041f3018fce700aa7f21742ffa17b
Gertjan Assies <g.assies@thenewmotion.com>
2015-01-27 15:25:26 +0100

    bla

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Build extends sbt.Build {
     settings = Project.defaultSettings ++ Seq(
       name                  := "akka-actor-statsd",
       organization          := "com.deploymentzone",
-      version               := "0.4-SNAPSHOT",
+      version               := "0.4dev-SNAPSHOT",
       licenses              := Seq("MIT" -> url("http://opensource.org/licenses/MIT")),
       homepage              := Some(url("https://github.com/cfeduke/akka-actor-statsd/")),
       scalaVersion          := CrossBuild.Versions.scala_211,
